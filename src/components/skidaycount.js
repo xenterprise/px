f25c09281664cc57129d0dad10c268b95453e361
XEnterprise <primesurgetechnologies@gmail.com>
2018-03-24 17:14:22 +0500

    Stateless components
    Stateless components to create components

--- src/components/skidaycount.js
+++ src/components/skidaycount.js
@@ -1,40 +1,38 @@
-import React from 'react'
 import '../stylesheets/ui.scss'
 var createReactClass = require('create-react-class');
 
 //export const TodoComponent = createReactClass({ render:function(){ return(<h1>Helloooo</h1>); } });
 
-export class SkiDayCount extends React.Component{
-    percentToDecimal(decimal){
-        return((decimal*100 )+'%')
-    }
+const percentToDecimal = (decimal) => {
+    return((decimal*100 )+'%')
+}
 
-    calcGoalProgress(total, goal){
-        return (this.percentToDecimal(total/goal))
-    }
-    render(){
-        return (
+const calcGoalProgress = (total, goal) => {
+    return (percentToDecimal(total/goal))
+}
+
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) =>(
             <div className="ski-day-count">
                 <div className="total-days">
-                    <span>{this.props.total}</span>
+                    <span>{total}</span>
                     <span> Days</span>
                 </div>
                 <div className="powder-days">
-                    <span>{this.props.powder}</span>
+                    <span>{powder}</span>
                     <span> Days</span>
                 </div>
                 <div className="backcountry-days">
-                    <span>{this.props.backcountry}</span>
+                    <span>{backcountry}</span>
                     <span> Days</span>
                 </div>
                 <div>
                     <span>
                     {
-                        this.calcGoalProgress(this.props.total, this.props.goal)
+                        calcGoalProgress(total, goal)
                     }
                     </span>
                 </div>
             </div>
         )
-    }
-}
+    
