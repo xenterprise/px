4e0d969b2a0bdd81d6242ee0c5bdfe8f25a44aa3
XEnterprise <primesurgetechnologies@gmail.com>
2018-03-26 00:50:36 +0500

    React Icons
    React Icons added,
    npm install --save react-icons

--- src/components/skidaycount.js
+++ src/components/skidaycount.js
@@ -1,6 +1,7 @@
 import '../stylesheets/ui.scss'
-var createReactClass = require('create-react-class');
-
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 //export const TodoComponent = createReactClass({ render:function(){ return(<h1>Helloooo</h1>); } });
 
 const percentToDecimal = (decimal) => {
@@ -16,14 +17,17 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) =>(
             <div className="ski-day-count">
                 <div className="total-days">
                     <span>{total}</span>
+                        <Calendar/>
                     <span> Days</span>
                 </div>
                 <div className="powder-days">
                     <span>{powder}</span>
+                        <SnowFlake/>
                     <span> Days</span>
                 </div>
                 <div className="backcountry-days">
                     <span>{backcountry}</span>
+                        <Terrain/>
                     <span> Days</span>
                 </div>
                 <div>
